8d41f4770d631a9c80c6d211ec9dfe9fbc432ac4
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-05-12 22:51:35 +0700

    First Commit Mantap

---
+++ application/helpers/app_helper.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+defined('APP_CREDENTIAL')      or define('APP_CREDENTIAL', 'laporanKaryawanMantap');
+defined('APP_SCREET')          or define('APP_SCREET', md5(APP_CREDENTIAL));
